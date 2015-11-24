99f074bfd1a43cb7d54297174b5f5a70670f0afd
Sunwoo Kim <kim392@illinois.edu>
2015-11-24 21:14:55 +0000

    Specify js docs included

---
+++ pages/urbanflow-jslib.php
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="<?=CGROOTURL . 'js/map/map-panel.js'?>"></script>
+<script type="text/javascript" src="<?=CGROOTURL . 'js/flum/uf-flum.js'?>"></script>
+<script type="text/javascript" src="<?=CGROOTURL . 'js/flum/Direction.js'?>"></script>
+<script type="text/javascript" src="<?=CGROOTURL . 'js/edgebundling/graph.js'?>"></script>
+<script type="text/javascript" src="<?=CGROOTURL . 'js/edgebundling/mingle2.js'?>"></script>
+<script type="text/javascript" src="<?=CGROOTURL . 'js/edgebundling/kdtree.js'?>"></script>
+<script type="text/javascript" src="<?=CGROOTURL . 'js/edgebundling/philogl.js'?>"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
